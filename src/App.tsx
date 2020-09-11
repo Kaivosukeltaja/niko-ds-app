1a333026e10b510454c62d45a67e616c04ee8bcb
Niko Salminen <niko@salminen.me>
2020-09-11 12:36:10 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { MyButton } from '@kaivosukeltaja/niko-ds-react';
 import './App.css';
 
 function App() {
@@ -10,14 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <MyButton icon="arrow-right.svg">Moi</MyButton>
       </header>
     </div>
   );
